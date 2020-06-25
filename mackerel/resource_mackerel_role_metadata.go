78be966476af782b89c0558e894f85a684bbdfc1
xcezx <maekawa@mercari.com>
2020-06-25 21:21:38 +0900

    re-impl resource_mackerel_role_metadata

--- mackerel/resource_mackerel_role_metadata.go
+++ mackerel/resource_mackerel_role_metadata.go
@@ -1,7 +1,13 @@
 package mackerel
 
 import (
+	"encoding/json"
+	"fmt"
+	"regexp"
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/structure"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/mackerelio/mackerel-client-go"
 )
 
@@ -11,7 +17,9 @@ func resourceMackerelRoleMetadata() *schema.Resource {
 		Read:   resourceMackerelRoleMetadataRead,
 		Update: resourceMackerelRoleMetadataUpdate,
 		Delete: resourceMackerelRoleMetadataDelete,
-
+		Importer: &schema.ResourceImporter{
+			State: resourceMackerelRoleMetadataImport,
+		},
 		Schema: map[string]*schema.Schema{
 			"service": {
 				Type:     schema.TypeString,
@@ -24,45 +32,56 @@ func resourceMackerelRoleMetadata() *schema.Resource {
 			"namespace": {
 				Type:     schema.TypeString,
 				Required: true,
+				ForceNew: true,
 			},
-			"metadata": {
-				Type:     schema.TypeMap,
-				Required: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
+			"metadata_json": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringIsJSON,
 			},
 		},
 	}
 }
 
 func resourceMackerelRoleMetadataCreate(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*mackerel.Client)
-	if err := client.PutRoleMetaData(
-		d.Get("service").(string),
-		d.Get("role").(string),
-		d.Get("namespace").(string),
-		d.Get("metadata").(mackerel.RoleMetaData),
-	); err != nil {
+	service := d.Get("service").(string)
+	role := d.Get("role").(string)
+	namespace := d.Get("namespace").(string)
+	var metadata mackerel.RoleMetaData
+	if err := json.Unmarshal([]byte(d.Get("metadata_json").(string)), &metadata); err != nil {
 		return err
 	}
-	d.SetId(d.Get("namespace").(string))
+
+	client := meta.(*mackerel.Client)
+	if err := client.PutRoleMetaData(service, role, namespace, metadata); err != nil {
+		return err
+	}
+	d.SetId(makeRoleMetadataID(service, role, namespace))
 	return resourceMackerelRoleMetadataRead(d, meta)
 }
 
 func resourceMackerelRoleMetadataRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*mackerel.Client)
-	resp, err := client.GetRoleMetaData(
-		d.Get("service").(string),
-		d.Get("role").(string),
-		d.Get("namespace").(string),
-	)
+
+	resp, err := client.GetRoleMetaData(d.Get("service").(string), d.Get("role").(string), d.Get("namespace").(string))
 	if err != nil {
 		return err
 	}
-	metadata := make(map[string]interface{})
-	for k, v := range resp.RoleMetaData.(map[string]interface{}) {
-		metadata[k] = v
+
+	metadataJSONBytes, err := json.Marshal(resp.RoleMetaData)
+	if err != nil {
+		return err
 	}
-	_ = d.Set("metadata", metadata)
+
+	metadataJSON, err := structure.NormalizeJsonString(string(metadataJSONBytes))
+	if err != nil {
+		return err
+	}
+
+	if err := d.Set("metadata_json", metadataJSON); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -72,13 +91,25 @@ func resourceMackerelRoleMetadataUpdate(d *schema.ResourceData, meta interface{}
 
 func resourceMackerelRoleMetadataDelete(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*mackerel.Client)
-	if err := client.DeleteRoleMetaData(
-		d.Get("service").(string),
-		d.Get("role").(string),
-		d.Get("namespace").(string),
-	); err != nil {
-		return err
+	return client.DeleteRoleMetaData(d.Get("service").(string), d.Get("role").(string), d.Get("namespace").(string))
+}
+
+func resourceMackerelRoleMetadataImport(d *schema.ResourceData, _ interface{}) ([]*schema.ResourceData, error) {
+	r := regexp.MustCompile(`^([a-zA-Z0-9-_]+)/roles/([a-zA-Z0-9-_]+)/metadata/(.*)$`)
+	if v := r.FindStringSubmatch(d.Id()); v != nil {
+		if err := d.Set("service", v[1]); err != nil {
+			return nil, err
+		}
+		if err := d.Set("role", v[2]); err != nil {
+			return nil, err
+		}
+		if err := d.Set("namespace", v[3]); err != nil {
+			return nil, err
+		}
 	}
-	d.SetId("")
-	return nil
+	return []*schema.ResourceData{d}, nil
+}
+
+func makeRoleMetadataID(service, role, namespace string) string {
+	return fmt.Sprintf("%s/roles/%s/metadata/%s", service, role, namespace)
 }
--- mackerel/resource_mackerel_role_metadata_test.go
+++ mackerel/resource_mackerel_role_metadata_test.go
@@ -11,27 +11,32 @@ import (
 )
 
 func TestAccMackerelRoleMetadata(t *testing.T) {
-	serviceName := fmt.Sprintf("tf-service-%s", acctest.RandString(5))
-	roleName := fmt.Sprintf("tf-role-%s", acctest.RandString(5))
-	namespace := fmt.Sprintf("tf-nt-%s", acctest.RandString(5))
+	resourceName := "mackerel_role_metadata.foo"
+	rand := acctest.RandString(5)
+	rServiceName := fmt.Sprintf("tf-%s", rand)
+	rRoleName := fmt.Sprintf("tf-%s-role", rand)
+	rNamespace := fmt.Sprintf("tf-namespace-%s", rand)
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: nil, // todo
 		Steps: []resource.TestStep{
+			// Test: Create
 			{
-				Config: testAccCheckMackerelRoleMetadataConfig(serviceName, roleName, namespace),
+				Config: testAccMackerelRoleMetadataConfig(rServiceName, rRoleName, rNamespace),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckMackerelRoleMetadataExists("mackerel_role_metadata.foo"),
-					resource.TestCheckResourceAttr(
-						"mackerel_role_metadata.foo", "service", serviceName),
-					resource.TestCheckResourceAttr(
-						"mackerel_role_metadata.foo", "role", roleName),
-					resource.TestCheckResourceAttr(
-						"mackerel_role_metadata.foo", "namespace", namespace),
-					resource.TestCheckResourceAttr(
-						"mackerel_role_metadata.foo", "metadata.TZ", "UTC"),
+					testAccCheckMackerelRoleMetadataExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "service", rServiceName),
+					resource.TestCheckResourceAttr(resourceName, "role", rRoleName),
+					resource.TestCheckResourceAttr(resourceName, "namespace", rNamespace),
 				),
+			},
+			// Test: Import
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
@@ -41,40 +46,40 @@ func testAccCheckMackerelRoleMetadataExists(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
 		if !ok {
-			return fmt.Errorf("role_metadata not found from resources: %s", n)
+			return fmt.Errorf("role metadata not found resources: %s", n)
 		}
 		if rs.Primary.ID == "" {
-			return fmt.Errorf("no role_metadata ID is set")
+			return fmt.Errorf("no role metadata ID is set")
 		}
 
 		client := testAccProvider.Meta().(*mackerel.Client)
-		_, err := client.GetRoleMetaData(rs.Primary.Attributes["service"], rs.Primary.Attributes["role"], rs.Primary.ID)
+		_, err := client.GetRoleMetaData(rs.Primary.Attributes["service"], rs.Primary.Attributes["role"], rs.Primary.Attributes["namespace"])
 		if err != nil {
-			return fmt.Errorf("err: %s", err)
+			return fmt.Errorf("role metadata not found mackerel: %s", rs.Primary.ID)
 		}
 		return nil
 	}
 }
 
-func testAccCheckMackerelRoleMetadataConfig(serviceName, roleName, namespace string) string {
+func testAccMackerelRoleMetadataConfig(serviceName, roleName, namespace string) string {
 	// language=HCL
 	return fmt.Sprintf(`
 resource "mackerel_service" "foo" {
-    name = "%s"
+	name = "%s"
 }
 
 resource "mackerel_role" "foo" {
-    service = "${mackerel_service.foo.id}"
-    name = "%s"
+	service = mackerel_service.foo.id
+	name = "%s"
 }
 
 resource "mackerel_role_metadata" "foo" {
-    service = "${mackerel_service.foo.id}"
-    role = "${mackerel_role.foo.id}"
-    namespace = "%s"
-    metadata = {
-        TZ = "UTC"
-    }
+	service = mackerel_service.foo.name
+	role = mackerel_role.foo.name
+	namespace = "%s"
+	metadata_json = jsonencode({
+		id = 1
+	})
 }
 `, serviceName, roleName, namespace)
 }
