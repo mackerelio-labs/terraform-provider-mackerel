1780af0503b181bdf74b33257f3a0404941bedaa
xcezx <maekawa@mercari.com>
2020-08-05 18:43:06 +0900

    implement datasource/channel

--- mackerel/provider.go
+++ mackerel/provider.go
@@ -29,6 +29,7 @@ func Provider() terraform.ResourceProvider {
 		},
 
 		DataSourcesMap: map[string]*schema.Resource{
+			"mackerel_channel":          dataSourceMackerelChannel(),
 			"mackerel_role":             dataSourceMackerelRole(),
 			"mackerel_role_metadata":    dataSourceMackerelRoleMetadata(),
 			"mackerel_service":          dataSourceMackerelService(),
