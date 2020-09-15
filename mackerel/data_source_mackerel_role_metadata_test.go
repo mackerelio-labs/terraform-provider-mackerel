3bfeaaf5901768beba05de6f703c166cccfe1952
xcezx <maekawa@mercari.com>
2020-09-15 20:16:38 +0900

    fix tests

--- mackerel/data_source_mackerel_role_metadata_test.go
+++ mackerel/data_source_mackerel_role_metadata_test.go
@@ -1,61 +1,62 @@
 package mackerel
 
-// import (
-// 	"fmt"
-// 	"testing"
+import (
+	"fmt"
+	"testing"
 
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-// )
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
 
-// func TestDataSourceMackerelRoleMetadata(t *testing.T) {
-// 	dsName := "data.mackerel_role_metadata.foo"
-// 	rand := acctest.RandString(5)
-// 	service := fmt.Sprintf("tf-service-%s", rand)
-// 	role := fmt.Sprintf("tf-role-%s", rand)
-// 	namespace := fmt.Sprintf("tf-namespace-%s", rand)
+func TestDataSourceMackerelRoleMetadata(t *testing.T) {
+	dsName := "data.mackerel_role_metadata.foo"
+	rand := acctest.RandString(5)
+	service := fmt.Sprintf("tf-service-%s", rand)
+	role := fmt.Sprintf("tf-role-%s", rand)
+	namespace := fmt.Sprintf("tf-namespace-%s", rand)
 
-// 	resource.ParallelTest(t, resource.TestCase{
-// 		PreCheck:          func() { testAccPreCheck(t) },
-// 		ProviderFactories: testAccProviderFactories,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config: testAccDataSourceMackerelRoleMetadataConfig(service, role, namespace),
-// 				Check: resource.ComposeTestCheckFunc(
-// 					resource.TestCheckResourceAttr(dsName, "id", fmt.Sprintf("%s:%s/%s", service, role, namespace)),
-// 					resource.TestCheckResourceAttr(dsName, "service", service),
-// 					resource.TestCheckResourceAttr(dsName, "role", role),
-// 					resource.TestCheckResourceAttr(dsName, "namespace", namespace),
-// 					resource.TestCheckResourceAttr(dsName, "metadata_json", `{"id":1}`),
-// 				),
-// 			},
-// 		},
-// 	})
-// }
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceMackerelRoleMetadataConfig(service, role, namespace),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dsName, "id", fmt.Sprintf("%s:%s/%s", service, role, namespace)),
+					resource.TestCheckResourceAttr(dsName, "service", service),
+					resource.TestCheckResourceAttr(dsName, "role", role),
+					resource.TestCheckResourceAttr(dsName, "namespace", namespace),
+					resource.TestCheckResourceAttr(dsName, "metadata_json", `{"id":1}`),
+				),
+			},
+		},
+	})
+}
 
-// func testAccDataSourceMackerelRoleMetadataConfig(service, role, namespace string) string {
-// 	return fmt.Sprintf(`
-// resource "mackerel_service" "foo" {
-//   name = "%s"
-// }
+func testAccDataSourceMackerelRoleMetadataConfig(service, role, namespace string) string {
+	return fmt.Sprintf(`
+resource "mackerel_service" "foo" {
+  name = "%s"
+}
 
-// resource "mackerel_role" "foo" {
-//   service = mackerel_service.foo.name
-//   name = "%s"
-// }
+resource "mackerel_role" "foo" {
+  service = mackerel_service.foo.name
+  name = "%s"
+}
 
-// resource "mackerel_role_metadata" "foo" {
-//   service = mackerel_role.foo.service
-//   role = mackerel_role.foo.name
-//   namespace = "%s"
-//   metadata_json = jsonencode({
-//     id = 1
-//   })
-// }
+resource "mackerel_role_metadata" "foo" {
+  service = mackerel_role.foo.service
+  role = mackerel_role.foo.name
+  namespace = "%s"
+  metadata_json = jsonencode({
+    id = 1
+  })
+}
 
-// data "mackerel_role_metadata" "foo" {
-//   service = mackerel_role_metadata.foo.service
-//   role = mackerel_role_metadata.foo.role
-//   namespace = mackerel_role_metadata.foo.namespace
-// }`, service, role, namespace)
-// }
+data "mackerel_role_metadata" "foo" {
+  depends_on = [mackerel_role_metadata.foo]
+  service = mackerel_role_metadata.foo.service
+  role = mackerel_role_metadata.foo.role
+  namespace = mackerel_role_metadata.foo.namespace
+}`, service, role, namespace)
+}
--- mackerel/data_source_mackerel_service_metadata_test.go
+++ mackerel/data_source_mackerel_service_metadata_test.go
@@ -1,52 +1,53 @@
 package mackerel
 
-// import (
-// 	"fmt"
-// 	"testing"
+import (
+	"fmt"
+	"testing"
 
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-// )
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
 
-// func TestAccDataSourceMackerelServiceMetadata(t *testing.T) {
-// 	dsName := "data.mackerel_service_metadata.foo"
-// 	rand := acctest.RandString(5)
-// 	serviceName := fmt.Sprintf("tf-service-%s", rand)
-// 	namespace := fmt.Sprintf("tf-namespace-%s", rand)
+func TestAccDataSourceMackerelServiceMetadata(t *testing.T) {
+	dsName := "data.mackerel_service_metadata.foo"
+	rand := acctest.RandString(5)
+	serviceName := fmt.Sprintf("tf-service-%s", rand)
+	namespace := fmt.Sprintf("tf-namespace-%s", rand)
 
-// 	resource.ParallelTest(t, resource.TestCase{
-// 		PreCheck:          func() { testAccPreCheck(t) },
-// 		ProviderFactories: testAccProviderFactories,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config: testAccDataSourceMackerelServiceMetadataConfig(serviceName, namespace),
-// 				Check: resource.ComposeTestCheckFunc(
-// 					resource.TestCheckResourceAttr(dsName, "service", serviceName),
-// 					resource.TestCheckResourceAttr(dsName, "namespace", namespace),
-// 					resource.TestCheckResourceAttr(dsName, "metadata_json", `{"id":1}`),
-// 				),
-// 			},
-// 		},
-// 	})
-// }
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceMackerelServiceMetadataConfig(serviceName, namespace),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dsName, "service", serviceName),
+					resource.TestCheckResourceAttr(dsName, "namespace", namespace),
+					resource.TestCheckResourceAttr(dsName, "metadata_json", `{"id":1}`),
+				),
+			},
+		},
+	})
+}
 
-// func testAccDataSourceMackerelServiceMetadataConfig(serviceName, namespace string) string {
-// 	return fmt.Sprintf(`
-// resource "mackerel_service" "foo" {
-//   name = "%s"
-// }
+func testAccDataSourceMackerelServiceMetadataConfig(serviceName, namespace string) string {
+	return fmt.Sprintf(`
+resource "mackerel_service" "foo" {
+  name = "%s"
+}
 
-// resource "mackerel_service_metadata" "foo" {
-//   service = mackerel_service.foo.name
-//   namespace = "%s"
-//   metadata_json = jsonencode({
-//     id = 1
-//   })
-// }
+resource "mackerel_service_metadata" "foo" {
+  service = mackerel_service.foo.name
+  namespace = "%s"
+  metadata_json = jsonencode({
+    id = 1
+  })
+}
 
-// data "mackerel_service_metadata" "foo" {
-//   service = mackerel_service_metadata.foo.service
-//   namespace = mackerel_service_metadata.foo.namespace
-// }
-// `, serviceName, namespace)
-// }
+data "mackerel_service_metadata" "foo" {
+  depends_on = [mackerel_service_metadata.foo]
+  service = mackerel_service_metadata.foo.service
+  namespace = mackerel_service_metadata.foo.namespace
+}
+`, serviceName, namespace)
+}
