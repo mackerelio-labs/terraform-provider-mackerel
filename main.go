a07dd9944391a5175bd46e5a22153dce11aeadac
hayajo_77 <hayajo_77@hatena.ne.jp>
2021-11-22 11:25:26 +0900

    add debug-mode

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
 	"github.com/mackerelio-labs/terraform-provider-mackerel/mackerel"
 )
 
+const (
+	providerAddr = "registry.terraform.io/mackerelio-labs/mackerel"
+)
+
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: mackerel.Provider})
+	var debug bool
+	flag.BoolVar(&debug, "debug", false, "run as debug-mode")
+
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
+		ProviderFunc: mackerel.Provider,
+	}
+
+	if debug {
+		if err := plugin.Debug(context.TODO(), providerAddr, opts); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
