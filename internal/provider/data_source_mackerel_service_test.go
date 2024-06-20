7764926029d60889204915071e0d7d0c28683ccf
Keisuke Nitta <tosukee@hatena.com>
2024-06-20 21:42:54 +0900

    test: add acctests for data source "mackerel_service"

--- internal/provider/data_source_mackerel_service_test.go
+++ internal/provider/data_source_mackerel_service_test.go
@@ -2,13 +2,19 @@ package provider_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	fwdatasource "github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/mackerelio-labs/terraform-provider-mackerel/internal/provider"
 )
 
-func TestMackerelServiceDataSourceSchema(t *testing.T) {
+func Test_MackerelServiceDataSource_schema(t *testing.T) {
 	t.Parallel()
 
 	ctx := context.Background()
@@ -23,4 +29,41 @@ func TestMackerelServiceDataSourceSchema(t *testing.T) {
 	if diags := resp.Schema.ValidateImplementation(ctx); diags.HasError() {
 		t.Fatalf("schema validation diagnostics: %+v", diags)
 	}
+}
+
+func TestAcc_MackerelServiceDataSource_basic(t *testing.T) {
+	name := fmt.Sprintf("tf-service-%s", acctest.RandString(5))
+	resourceName := "data.mackerel_service.foo"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { preCheck(t) },
+		ProtoV5ProviderFactories: protoV5ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: mackerelServiceDataSourceConfig(name),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("id"), knownvalue.StringExact(name)),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("name"), knownvalue.StringExact(name)),
+					statecheck.ExpectKnownValue(
+						resourceName,
+						tfjsonpath.New("memo"),
+						knownvalue.StringExact("This service is managed by Terraform."),
+					),
+				},
+			},
+		},
+	})
+}
+
+func mackerelServiceDataSourceConfig(name string) string {
+	return fmt.Sprintf(`
+resource "mackerel_service" "foo" {
+  name = "%s"
+  memo = "This service is managed by Terraform."
+}
+
+data "mackerel_service" "foo" {
+  name = mackerel_service.foo.id
+}
+`, name)
 }
