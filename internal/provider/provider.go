879267140fa049074c89b29d175d77bd109e149d
Keisuke Nitta <tosukee@hatena.com>
2024-06-20 21:37:40 +0900

    feat: reimplement data source "mackerel_service_metadata"

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -91,6 +91,7 @@ func (m *mackerelProvider) Resources(context.Context) []func() resource.Resource
 func (m *mackerelProvider) DataSources(context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{
 		NewMackerelServiceDataSource,
+		NewMackerelServiceMetadataDataSource,
 	}
 }
 
