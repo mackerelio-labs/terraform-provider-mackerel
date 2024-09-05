ee37709e7c87a96d90aeadf7639c4ad3d4b17008
Keisuke Nitta <tosukee@hatena.com>
2024-09-05 18:03:44 +0900

    feat: reimplement "mackerel_dashboard" resource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -84,6 +84,7 @@ func (m *mackerelProvider) Configure(ctx context.Context, req provider.Configure
 func (m *mackerelProvider) Resources(context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
 		NewMackerelChannelResource,
+		NewMackerelDashboardResource,
 		NewMackerelMonitorResource,
 		NewMackerelNotificationGroupResource,
 		NewMackerelRoleResource,
