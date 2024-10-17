5f6fb00523479b4128f235d0de1e81e58b6de2fa
Keisuke Nitta <tosukee@hatena.com>
2024-10-17 18:33:58 +0900

    test: convert to API

--- internal/mackerel/aws_integration_test.go
+++ internal/mackerel/aws_integration_test.go
@@ -264,3 +264,148 @@ func Test_AWSIntegration_fromAPI(t *testing.T) {
 		})
 	}
 }
+
+func Test_AWSIntegration_toAPI(t *testing.T) {
+	t.Parallel()
+
+	cases := map[string]struct {
+		model AWSIntegrationModel
+		param mackerel.CreateAWSIntegrationParam
+	}{
+		"basic": {
+			model: AWSIntegrationModel{
+				ID:           types.StringValue("aaaabbbb"),
+				Name:         types.StringValue("aws-integration"),
+				Memo:         types.StringValue("This resource is managed by Terraform."),
+				Key:          types.StringValue(""),
+				SecretKey:    types.StringNull(),
+				RoleARN:      types.StringValue("arn:aws:iam::11111111:role/MackerelAWSIntegrationRole"),
+				ExternalID:   types.StringValue("ccccddddd"),
+				Region:       types.StringValue("ap-northeast-1"),
+				IncludedTags: types.StringValue("Name:production-server,Environment:production"),
+				ExcludedTags: types.StringValue("Name:staging-server,Environment:staging"),
+
+				EC2: []AWSIntegrationServiceWithRetireAutomatically{{
+					Enable:              types.BoolValue(true),
+					ExcludedMetrics:     []string{},
+					RetireAutomatically: types.BoolValue(true),
+				}},
+				ELB: []AWSIntegrationService{},
+				ALB: []AWSIntegrationService{{
+					Enable:              types.BoolValue(true),
+					Role:                types.StringValue("service: role"),
+					ExcludedMetrics:     []string{"alb.request.count", "alb.bytes.processed"},
+					RetireAutomatically: types.BoolValue(false),
+				}},
+				NLB: []AWSIntegrationService{{
+					Enable:              types.BoolValue(true),
+					Role:                types.StringNull(),
+					ExcludedMetrics:     []string{},
+					RetireAutomatically: types.BoolValue(false),
+				}},
+				RDS: []AWSIntegrationServiceWithRetireAutomatically{{
+					Enable:              types.BoolValue(true),
+					Role:                types.StringValue("service: role"),
+					ExcludedMetrics:     []string{"rds.cpu.used"},
+					RetireAutomatically: types.BoolValue(false),
+				}},
+				Redshift:    []AWSIntegrationService{},
+				ElastiCache: []AWSIntegrationServiceWithRetireAutomatically{},
+				SQS:         []AWSIntegrationService{},
+				Lambda: []AWSIntegrationService{{
+					Enable:              types.BoolValue(true),
+					Role:                types.StringNull(),
+					ExcludedMetrics:     []string{},
+					RetireAutomatically: types.BoolValue(false),
+				}},
+				DynamoDB:   []AWSIntegrationService{},
+				CloudFront: []AWSIntegrationService{},
+				APIGateway: []AWSIntegrationService{},
+				Kinesis:    []AWSIntegrationService{},
+				S3:         []AWSIntegrationService{},
+				ES:         []AWSIntegrationService{},
+				ECSCluster: []AWSIntegrationService{},
+				SES:        []AWSIntegrationService{},
+				States:     []AWSIntegrationService{},
+				EFS:        []AWSIntegrationService{},
+				Firehose:   []AWSIntegrationService{},
+				Batch:      []AWSIntegrationService{},
+				WAF:        []AWSIntegrationService{},
+				Billing:    []AWSIntegrationService{},
+				Route53:    []AWSIntegrationService{},
+				Connect:    []AWSIntegrationService{},
+				DocDB:      []AWSIntegrationService{},
+				CodeBuild:  []AWSIntegrationService{},
+			},
+			param: mackerel.CreateAWSIntegrationParam{
+				Name:         "aws-integration",
+				Memo:         "This resource is managed by Terraform.",
+				Key:          "",
+				SecretKey:    "",
+				RoleArn:      "arn:aws:iam::11111111:role/MackerelAWSIntegrationRole",
+				ExternalID:   "ccccddddd",
+				Region:       "ap-northeast-1",
+				IncludedTags: "Name:production-server,Environment:production",
+				ExcludedTags: "Name:staging-server,Environment:staging",
+				Services: map[string]*mackerel.AWSIntegrationService{
+					"EC2": {
+						Enable:              true,
+						ExcludedMetrics:     []string{},
+						RetireAutomatically: true,
+					},
+					"ELB": {ExcludedMetrics: []string{}},
+					"ALB": {
+						Enable:          true,
+						Role:            ptr("service: role"),
+						ExcludedMetrics: []string{"alb.request.count", "alb.bytes.processed"},
+					},
+					"NLB": {
+						Enable:          true,
+						ExcludedMetrics: []string{},
+					},
+					"RDS": {
+						Enable:          true,
+						Role:            ptr("service: role"),
+						ExcludedMetrics: []string{"rds.cpu.used"},
+					},
+					"Redshift":    {ExcludedMetrics: []string{}},
+					"ElastiCache": {ExcludedMetrics: []string{}},
+					"SQS":         {ExcludedMetrics: []string{}},
+					"Lambda": {
+						Enable:          true,
+						ExcludedMetrics: []string{},
+					},
+					"DynamoDB":   {ExcludedMetrics: []string{}},
+					"CloudFront": {ExcludedMetrics: []string{}},
+					"APIGateway": {ExcludedMetrics: []string{}},
+					"Kinesis":    {ExcludedMetrics: []string{}},
+					"S3":         {ExcludedMetrics: []string{}},
+					"ES":         {ExcludedMetrics: []string{}},
+					"ECSCluster": {ExcludedMetrics: []string{}},
+					"SES":        {ExcludedMetrics: []string{}},
+					"States":     {ExcludedMetrics: []string{}},
+					"EFS":        {ExcludedMetrics: []string{}},
+					"Firehose":   {ExcludedMetrics: []string{}},
+					"Batch":      {ExcludedMetrics: []string{}},
+					"WAF":        {ExcludedMetrics: []string{}},
+					"Billing":    {ExcludedMetrics: []string{}},
+					"Route53":    {ExcludedMetrics: []string{}},
+					"Connect":    {ExcludedMetrics: []string{}},
+					"DocDB":      {ExcludedMetrics: []string{}},
+					"CodeBuild":  {ExcludedMetrics: []string{}},
+				},
+			},
+		},
+	}
+
+	for name, tt := range cases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			param := tt.model.createParam()
+			if diff := cmp.Diff(param, &tt.param); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
